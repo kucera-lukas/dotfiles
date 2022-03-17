b121738ae7d9808020ce9cd86fe64a86f7beaeaf
Lukas Kucera <kucera-lukas@users.noreply.github.com>
2022-03-17 13:42:55 +0100

    tweak config.fish

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -2,6 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# cargo
+set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
+
+# asdf completions
 source ~/.asdf/asdf.fish
 
 # Better cd
