5195cc2d6dd31b0691b370eb2ac0a040215f2123
Lukas Kucera <85391931+kucera-lukas@users.noreply.github.com>
2022-08-09 15:40:33 +0200

    Sync Bitwarden configuration (#7)

--- home/dot_config/private_fish/functions/battlenet.fish
+++ home/dot_config/private_fish/functions/battlenet.fish
@@ -1,6 +1,6 @@
 function battlenet --description 'Open Blizzard Battle.net'
     # fix for Hearthstone tavern door infinite loading
-    remove -f Games/battlenet/drive_c/Program\ Files\ \(x86\)/Hearthstone/Hearthstone_Data/Plugins/x86/LocationAPI.dll
+    remove -f ~/Games/battlenet/drive_c/Program\ Files\ \(x86\)/Hearthstone/Hearthstone_Data/Plugins/x86/LocationAPI.dll
 
     lutris battlenet
 end
