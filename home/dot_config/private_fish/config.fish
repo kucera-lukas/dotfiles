8662afb53753b9098369ab5470ca35059409b353
Lukas Kucera <85391931+kucera-lukas@users.noreply.github.com>
2022-11-29 23:08:23 +0100

    clean up shell profile dotfiles

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -5,9 +5,6 @@ end
 # locale
 set -gx LC_ALL en_US.UTF-8
 
-# cargo
-set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
-
 # asdf completions
 source ~/.asdf/asdf.fish
 
@@ -22,9 +19,6 @@ navi widget fish | source
 
 # load and unload environment variables
 direnv hook fish | source
-
-# flyctl
-set -x PATH $HOME/.fly/bin $PATH
 
 # pack
 source (pack completion --shell fish)
