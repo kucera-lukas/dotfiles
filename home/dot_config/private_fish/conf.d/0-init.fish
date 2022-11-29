5f3878a91287eaa351722fe1654ce44dec4c61cf
Lukas Kucera <85391931+kucera-lukas@users.noreply.github.com>
2022-11-29 23:26:09 +0100

    clean up shell profile dotfiles (#31)

--- home/dot_config/private_fish/conf.d/0-init.fish
+++ home/dot_config/private_fish/conf.d/0-init.fish
@@ -1,1 +1,7 @@
-set -x PATH $HOME/.local/bin $PATH
+set -U fish_user_paths $HOME/.local/bin $fish_user_paths
+
+# cargo
+set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
+
+# flyctl
+set -U fish_user_paths $HOME/.fly/bin $fish_user_paths
